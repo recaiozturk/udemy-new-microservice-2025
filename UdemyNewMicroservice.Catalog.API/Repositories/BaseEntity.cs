e758d19346d1d7fbc9cd9600918cb4c92eae512a
recaiozturk <recaiozturk54@gmail.com>
2025-11-15 10:14:33 +0300

    mongo db,provider setup

---
+++ UdemyNewMicroservice.Catalog.API/Repositories/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace UdemyNewMicroservice.Catalog.API.Repositories
+{
+    public class BaseEntity
+    {
+        [BsonElement("_id")]
+        public Guid Id { get; set; } //snow flake
+    }
+}
