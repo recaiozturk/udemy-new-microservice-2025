196c5c106e43498b14f237e7335132d1774569d6
recaiozturk <recaiozturk54@gmail.com>
2025-12-27 09:50:13 +0300

    added Categories Create commandHandler,response

---
+++ UdemyNewMicroservice.Catalog.API/Features/Categories/Create/CreateCategoryCommand.cs
@@ -0,0 +1,26 @@
+﻿
+using MediatR;
+using UdemyNewMicroservice.Shared;
+
+namespace UdemyNewMicroservice.Catalog.API.Features.Categories.Create
+{
+    public record  CreateCategoryCommand(string Name) :IRequest<ServiceResult<CreateCategoryResponse>>;
+
+
+
+
+    //record ile inmotable objectlar oluşturabiliriz
+    //yani nesne oluşturulduktan sonra değiştirilemez
+
+    //yukardaki kısa süümüdür aşağıdaki uzun sürümüde yazabilirdik, .net 8 ile gelmiştir
+
+    //public class CreateCategoryCommandHandler
+    //{
+    //    public string Name { get; init; }
+
+    //    public CreateCategoryCommandHandler(string name)
+    //    {
+    //        Name = name;
+    //    }
+    //}
+}
---
+++ UdemyNewMicroservice.Catalog.API/Features/Categories/Create/CreateCategoryCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using UdemyNewMicroservice.Catalog.API.Repositories;
+using UdemyNewMicroservice.Shared;
+
+namespace UdemyNewMicroservice.Catalog.API.Features.Categories.Create
+{
+    public class CreateCategoryCommandHandler(AppDbContext context) : IRequestHandler<CreateCategoryCommand, ServiceResult<CreateCategoryResponse>>
+    {
+        public async Task<ServiceResult<CreateCategoryResponse>> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var existingCategory=await context.Categories.AnyAsync(x=>x.Name==request.Name,cancellationToken);
+        }
+    }
+}
